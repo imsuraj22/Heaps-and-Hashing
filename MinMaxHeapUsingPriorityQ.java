6fa19d433b48bf42cf21dc9b96e9203f095c1c86
Suraj <surajgaikwad0287@gmail.com>
2022-10-08 12:43:16 +0530

    done

--- MinMaxHeapUsingPriorityQ.java
+++ MinMaxHeapUsingPriorityQ.java
@@ -5,7 +5,7 @@ public class MinMaxHeapUsingPriorityQ {
 
     static void MinHeap(){
         PriorityQueue<Integer> q=new PriorityQueue<>();
-        q.add(4);
+        q.add(4); 
         q.add(2);
         q.add(5);
         q.add(3);
